8123585a8192c2d256f1b21db061061c72aee631
gerep <daniel.gerep@gmail.com>
2014-10-31 16:48:51 -0200

    Starting :metal:

---
+++ melancholia.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "log"
+)
+
+type ApiFunc func(w http.ResponseWriter, r *http.Request)
+
+func main() {
+  createRoutes()
+
+  log.Println("Listening on :4242")
+  http.ListenAndServe(":4242", nil)
+}
+
+func createRoutes() {
+  m := map[string]map[string]ApiFunc{
+    "GET": {
+      "/ping": ping,
+      "/pong": pong,
+    },
+  }
+
+  for method, routes := range m {
+    for route, function := range routes {
+      log.Printf("Creating route %s with method %s", route, method)
+      createHandler(method, route, function)
+    }
+  }
+}
+
+func createHandler(method string, route string, function ApiFunc) {
+  http.HandleFunc(route, function)
+}
+
+func ping(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "I'm ping @ %s", r.URL.Path[1:])
+}
+
+func pong(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "I'm pong @ %s", r.URL.Path[1:])
+}
