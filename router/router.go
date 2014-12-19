4b4872b7799b3deb521baed6fafe9239c534e4b0
gerep <daniel.gerep@gmail.com>
2014-12-19 18:57:35 -0200

    Fixes some errors and remove unused code

--- router/router.go
+++ router/router.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"encoding/json"
 	"net/http"
-	"strconv"
 
 	"github.com/gerep/melancholia/models"
 	"github.com/gorilla/mux"
@@ -23,17 +22,8 @@ func (r Router) CreateRoutes() {
 	http.Handle("/", gmux)
 
 	m := map[string]map[string]ApiFunc{
-		"GET": {
-			"/users/{id:[0-9]+}": r.getUser,
-		},
 		"POST": {
 			"/users": r.createUser,
-		},
-		"DELETE": {
-			"/users": deleteUser,
-		},
-		"PUT": {
-			"/users": updateUser,
 		},
 	}
 
@@ -41,19 +31,6 @@ func (r Router) CreateRoutes() {
 		for route, function := range routes {
 			gmux.HandleFunc(route, function).Methods(method)
 		}
-	}
-}
-
-func (r Router) getUser(w http.ResponseWriter, req *http.Request) {
-	vars := mux.Vars(req)
-	id := vars["id"]
-	user := models.User{DB: r.DB, Id: id}
-	result, err := user.Get()
-
-	if err != nil {
-		json.NewEncoder(w).Encode(err)
-	} else {
-		json.NewEncoder(w).Encode(result)
 	}
 }
 
@@ -70,12 +47,4 @@ func (r Router) createUser(w http.ResponseWriter, req *http.Request) {
 	} else {
 		json.NewEncoder(w).Encode(result)
 	}
-}
-
-func updateUser(w http.ResponseWriter, req *http.Request) {
-	//
-}
-
-func deleteUser(w http.ResponseWriter, req *http.Request) {
-	//
 }
